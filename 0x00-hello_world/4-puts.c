9580a3135cd2b1f1d31b726c104e4a1944ea5a7e
osama salah <osamaaboalhassan20@gmail.com>
2023-02-19 14:12:49 -0800

    333

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts functio
+ n 
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\" Programming is like building a multilingual puzzle");
+return (0); 
+}
+
