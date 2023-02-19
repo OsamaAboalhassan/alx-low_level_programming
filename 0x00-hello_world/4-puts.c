3231b55a122a234ad6786378bccbd3bd06200a53
osama salah <osamaaboalhassan20@gmail.com>
2023-02-19 14:15:40 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - A program that prints a line with puts functio
- n 
- * Return: 0 (Success)
+ * main - Entry point
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
  */
+
 int main(void)
 {
-puts("\" Programming is like building a multilingual puzzle");
-return (0); 
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
