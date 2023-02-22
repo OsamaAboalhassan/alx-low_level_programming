84d77954625bda870921926b2355a7a7ecb3036a
osama salah <osamaaboalhassan20@gmail.com>
2023-02-21 18:22:25 -0800

    22222

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,24 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
- * followed by a new line
+ * print_alphabet_x10 - Make alphabet x10 times
+ *
+ * Return: void
  */
 void print_alphabet_x10(void)
 {
-        char ch;
-        int i;
-        i = 0;
-        while (i < 10)
-        {
-                ch = 'a';
-                while (ch <= 'z')
-                {
-                        _putchar(ch);
-                        ch++;
-                }
-                _putchar('\n');
-                i++;
-        }
+	char c;
+	int i = 0;
+
+	while (i <= 9)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+
+		_putchar('\n');
+
+		i++;
+	}
 }
