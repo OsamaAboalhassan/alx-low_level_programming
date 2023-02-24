049e119e84a868e0ec49e5d4142e98e5cb0c0872
osama salah <osamaaboalhassan20@gmail.com>
2023-02-24 08:51:52 -0800

    111

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * _isdigit - check if numbers are 0 - 9
- * @c: char to be checked
- *
- * Return: 0 or 1
+ * _isdigit - checks if a character is digit
+ * @c: carrier variable
+ * Return: 1 if true, 0 if false
  */
 
 int _isdigit(int c)
 {
-        if (c >= '0' && c <= '9')
-                return (1);
-        else
-                return (0);
-}     
+	if (c >= '0' && c <= '9')
+		return (1);
+
+	else
+		return (0);
+}
